71277235873c7ab78e17d47095b9ee3699a30cd0
Shivpoojan Rajak <52307455+shiva-sudo@users.noreply.github.com>
2022-10-17 15:56:05 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -6,7 +6,7 @@ public class ReverseNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int number = sc.nextInt(); 
 		int reverse = 0; 
