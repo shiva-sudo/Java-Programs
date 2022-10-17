b38dc3667ffc870f782ebecbcccf560ad3f61faa
Shivpoojan Rajak <52307455+shiva-sudo@users.noreply.github.com>
2022-10-17 15:50:27 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+package javatraining;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		int number = sc.nextInt(); 
+		int reverse = 0; 
+		while(number != 0)
+		{
+		int remainder = number % 10;
+		reverse = reverse * 10 + remainder;
+		number = number/10;
+		// 0  System.out.println (reverse);
+		}
+
+	}
+
+}
